fb76ee91b8214c996f4dbe98e45a9f266fc3fd81
Graham Marlow <graham@mgmarlow.com>
2024-09-20 15:00:18 -0700

    Initial commit

--- src/game.ts
+++ src/game.ts
@@ -1,17 +1,105 @@
-class Game {
+const levelOne = [
+  "p......",
+  ".......",
+  "g......",
+  "www....",
+  "g.w....",
+  ".......",
+  "..w....",
+];
+
+// TEMP
+const PLAYER = "p";
+const WALL = "w";
+const EMPTY = ".";
+const GHOST = "g";
+
+export type Tile = typeof PLAYER | typeof EMPTY | typeof WALL | typeof GHOST;
+
+// TODO: Eventually might want to move the player to its own index
+// or its own layer, since the player can perhaps overlap with certain
+// tiles (e.g. items).
+export interface Level {
+  width: number;
+  height: number;
+  tiles: Tile[][];
+}
+
+export const createLevel = (raw: string[]): Level => {
+  let height = raw.length;
+  // Assumed square levels.
+  let width = raw[0].length;
+
+  const tiles: Tile[][] = raw.reduce((acc, cur) => {
+    const row: Tile[] = [];
+    for (let i = 0; i < cur.length; i++) {
+      const token = cur[i];
+      // TODO: validate token.
+      row.push(token as Tile);
+    }
+
+    acc.push(row);
+
+    return acc;
+  }, [] as Tile[][]);
+
+  return {
+    width,
+    height,
+    tiles,
+  };
+};
+
+class Point {
   constructor(
-    private render: () => void,
-    public count = 0,
+    public col: number,
+    public index: number,
   ) {}
 
-  increment() {
-    this.count++;
-    this.render();
+  add(other: Point): Point {
+    return new Point(this.col + other.col, this.index + other.index);
+  }
+}
+
+const move = (origin: Point): Point[] => {
+  const dirs = [
+    { col: 0, index: 1 },
+    { col: 0, index: 2 },
+    { col: 1, index: 0 },
+    { col: 2, index: 0 },
+    { col: 0, index: -1 },
+    { col: 0, index: -2 },
+    { col: -1, index: 0 },
+    { col: -2, index: 0 },
+  ];
+
+  return dirs.flatMap((dir) => {
+    const result = new Point(dir.col, dir.index).add(origin);
+
+    // TODO: Need to check level width/height as well.
+    if (
+      result.col >= 0 &&
+      result.col <= 7 &&
+      result.index <= 7 &&
+      result.index >= 0
+    ) {
+      return result;
+    } else {
+      // Lazy filterMap.
+      return [];
+    }
+  });
+};
+
+class Game {
+  level: Level;
+
+  constructor(private render: () => void) {
+    this.level = createLevel(levelOne);
   }
 
-  decrement() {
-    this.count--;
-    this.render();
+  get tiles(): Tile[][] {
+    return this.level.tiles;
   }
 }
 
--- src/view.ts
+++ src/view.ts
@@ -1,10 +1,80 @@
 import { h } from "snabbdom";
-import Game from "./game";
+import Game, { type Tile } from "./game";
+import player from "./img/player.png";
+import ghost from "./img/ghost.png";
+import wall from "./img/wall.png";
+
+const TILE_SIZE = 64;
+
+const tileSrc = (token: string): string | undefined => {
+  switch (token) {
+    case "p":
+      return player;
+    case "g":
+      return ghost;
+    case "w":
+      return wall;
+  }
+};
+
+const Board = (tiles: Tile[][]) => {
+  const content = tiles.map((row) => {
+    const rowContent = row.map((token: Tile) => {
+      const tileContent = [];
+      const src = tileSrc(token);
+
+      if (src) {
+        tileContent.push(
+          h("img", {
+            attrs: { src },
+            style: {
+              imageRendering: "pixelated",
+              width: "100%",
+              height: "100%",
+            },
+          }),
+        );
+      }
+
+      return h(
+        "div.square",
+        {
+          style: {
+            backgroundColor: "#b58863",
+            display: "inline-block",
+            width: `${TILE_SIZE}px`,
+            height: `${TILE_SIZE}px`,
+          },
+        },
+        tileContent,
+      );
+    });
+
+    return h("div.rank", { style: { display: "flex" } }, rowContent);
+  });
+
+  return h("div.board", content);
+};
+
+const Sidebar = (game: Game) => {
+  return h(
+    "div.sidebar",
+    {
+      style: {
+        padding: "1rem",
+        backgroundColor: "#161512",
+        display: "flex",
+        flexDirection: "column",
+      },
+    },
+    [h("button", "move"), h("button", "attack")],
+  );
+};
 
 export const view = (game: Game) => {
-  return h("div", [
-    h("p", `count: ${game.count}`),
-    h("button", { on: { click: () => game.increment() } }, "+"),
-    h("button", { on: { click: () => game.decrement() } }, "-"),
-  ]);
+  return h(
+    "div.container",
+    { style: { display: "flex", justifyContent: "center" } },
+    [Board(game.tiles), Sidebar(game)],
+  );
 };
